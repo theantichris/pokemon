2eb968658ab473174d347cec06853f0145b31f08
Christopher Lamm <chris@theantichris.com>
2019-11-29 09:36:40 -0500

    using potions

--- pokemon.py
+++ pokemon.py
@@ -1,5 +1,5 @@
 class Pokemon:
-    def __init__(self, name, type, level = 1):
+    def __init__(self, name, type, level=1):
         self.name = name
         self.type = type
         self.level = level
@@ -17,7 +17,8 @@ class Pokemon:
             self.current_health = 0
             self.knock_out()
         else:
-            print("{} now has {} health.".format(self.name, self.current_health))
+            print("{} now has {} health.".format(
+                self.name, self.current_health))
 
     def gain_health(self, health):
         print("{} healed {} damage.".format(self.name, health))
@@ -28,11 +29,11 @@ class Pokemon:
         if self.current_health > self.max_health:
             self.current_health = self.max_health
         print("{} now has {} health.".format(self.name, self.current_health))
-    
+
     def knock_out(self):
         self.is_knocked_out = True
         print("{} has been knocked out.".format(self.name))
-    
+
     def revive(self):
         self.is_knocked_out = False
         if self.current_health <= 0:
@@ -46,17 +47,21 @@ class Pokemon:
 
         if (self.type == "Fire" and other_pokemon.type == "Water") or (self.type == "Water" and other_pokemon.type == "Grass") or (self.type == "Grass" and other_pokemon.type == "Fire"):
             damage = round(self.level * .05)
-            print("{} attacked {} for {} damage.".format(self.name, other_pokemon.name, damage))
+            print("{} attacked {} for {} damage.".format(
+                self.name, other_pokemon.name, damage))
             print("It's not very effective.")
         elif (self.type == "Fire" and other_pokemon.type == "Grass") or (self.type == "Water" and other_pokemon.type == "Fire") or (self.type == "Grass" and other_pokemon.type == "Water"):
             damage = self.level * 2
-            print("{} attacked {} for {} damage.".format(self.name, other_pokemon.name, damage))
+            print("{} attacked {} for {} damage.".format(
+                self.name, other_pokemon.name, damage))
             print("It's super effective!")
         else:
             damage = self.level
-            print("{} attacked {} for {} damage.".format(self.name, other_pokemon.name, damage))
+            print("{} attacked {} for {} damage.".format(
+                self.name, other_pokemon.name, damage))
 
         other_pokemon.lose_health(damage)
+
 
 class Trainer:
     def __init__(self, name, pokemon_list, num_potions):
@@ -71,8 +76,19 @@ class Trainer:
             print(pokemon)
         return "Their current active Pokemon is {}".format(self.pokemon_list[self.current_pokemon].name)
 
+    def use_potion(self):
+        if self.num_potions <= 0:
+            print("{} does not have any potions.".format(self.name))
+            return
+
+        print("{} used a potion on {}.".format(self.name, self.pokemon_list[self.current_pokemon].name))
+        self.pokemon_list[self.current_pokemon].gain_health(20)
+        self.num_potions -= 1
+
+
 squirtle = Pokemon("Squirtle", "Water")
 charmander = Pokemon("Charmander", "Fire")
-christopher = Trainer("Christopher", [squirtle, charmander], 10)
+trainer = Trainer("Christopher", [squirtle, charmander], 1)
 
-print(christopher)
+trainer.use_potion()
+trainer.use_potion()
